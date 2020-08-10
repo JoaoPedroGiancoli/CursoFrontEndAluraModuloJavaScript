341e1f6b2290df202bd4c06754a1ddce467601c0
JoaoPedroGiancoli <joaopedrogiancoli12@gmail.com>
2020-08-10 13:32:00 -0300

    first commit

---
+++ ContaCorrente.js
@@ -0,0 +1,51 @@
+import { Cliente } from "./Cliente.js";
+
+export class ContaCorrente{
+    static numeroDeContas = 0;
+    agencia;
+    #cliente;
+
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente){
+
+            this.#cliente = novoValor;
+        }
+    }
+
+    get cliente(){
+        return this.#cliente;
+    }
+
+        //private
+    #saldo;
+
+    get saldo(){
+        return this.#saldo
+    }
+    
+    constructor(agencia, cliente){
+        this.agencia = agencia;
+        this.cliente = cliente;
+        ContaCorrente.numeroDeContas += 1;
+    }
+
+    sacar(valor){
+        
+        if(this.#saldo >= valor){
+        
+            this.#saldo -= valor;
+            return valor;
+         }
+    }
+
+    depositar(valor){
+        if(valor <= 0) return;
+        this.#saldo += valor;
+    }
+
+    transferir(valor, conta) {
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
+
+    }
+}
---
+++ index.js
@@ -0,0 +1,14 @@
+import {Cliente} from "./Cliente.js"
+import {ContaCorrente} from "./ContaCorrente.js"
+
+const cliente1 = new Cliente("Ricando",111222333309);
+
+const cliente2 = new Cliente("Alice",88822233309);
+
+const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
+contaCorrenteRicardo.depositar(500);
+
+const conta2 = new ContaCorrente(102, cliente2);
+
+
+console.log(ContaCorrente.numeroDeContas);
